908691de90e5661bb93e156e682acd9120af7fb8
Rubber-Ball <danielindiestudio@gmail.com>
2025-07-12 23:45:56 +0300

    make pages work and adding Redirect file to redirect to block-engine-html.

--- Block-Engine-HTML/index.php
+++ Block-Engine-HTML/index.php
@@ -1,1 +1,75 @@
 
+<?php?>
+<html lang="en">
+<head>
+  <link rel="icon" type="image/png+xml" href="Images/Brick.webp">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Block-Engine Home</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<!-- example
+  <form method="get" action="Files/Block-Engine-Linux.zip" >
+    <button type="submit">hello</button>
+  </form>
+-->
+
+  <body class="background">
+
+    <h1>
+      <img alt="Brick.png" src="Images/Brick.png" style="position: absolute;left: 50%;top: 20px;transform: translateX(-50%);overlay:auto;">
+    </h1>
+
+    <h1 class="Block-Engine">
+      Block Engine
+    </h1>
+
+    <h2 style="position: absolute;top: 150px;left: 50%;" class="font">Block-Engine is made for game devs!</h2>
+    
+    <p style="position: absolute;top: 250px;left: 50%; white-space: nowrap;" class="p">
+    Hello i'm Rubber-Ball / Daniel
+    </p>
+
+    <hr style="position: absolute;top: 370px;left: -100%;">
+
+  <!--\\\\\\\\\\\\\\\\\\\\\\\\\ how did i learn ? ///////////////////-->
+    <h1 style="position: absolute;top: 400px;left: 50%;" class="font">
+      Were / How did i learn ?
+    </h1>
+
+    <p style="position: absolute;top: 500px;left: 50%;" class="p">
+      i watched bro code he is really good i almost learned every programing language from him !
+    </p>
+  <!--\\\\\\\\\\\\\\\\\\\\\\\\\ Why C ? /////////////////////// -->
+    <h1 style="position: absolute;top: 770px;left: 50%;" class="font">
+      Why C ?
+    </h1>
+
+    <p style="position: absolute;top: 850px;left: 50%;" class="p">
+      because it's fun not like C++ and it's simple when you learn not just copy and paste !
+    </p>
+
+    <h1 style="position: absolute;top: 1100px;left: 50%;" class="font">
+      What programing languages do i know ?
+    </h1>
+
+    <p style="position: absolute;top: 1200px;left: 50%;" class="p">
+      HTML5 | CSS | OpenGL | GDScript | C | Some Python 
+    </p>
+
+    <h1 style="position: absolute;top: 1400px;left: 52%;width: 800px;" class="font">
+      The best time to learn programing was 10y ago the second best time is Now !
+    </h1>
+
+    <p style="position: absolute;top: 1500px;left: 50%;" class="p">
+      so i started on the age of 10y on my phone ! you're mind is the limit !.
+    </p>
+
+
+  <h2 style="position: absolute;left: 38%;top:1700px; margin-top: 3%;margin-bottom: 3%;">you can report bugs at accsumb@gmail.com</h2> 
+
+  <h6 style="position: absolute;left: 38%;top:1850px; margin-top: 3%;margin-bottom: 3%;">.</h6>
+
+  </body>
+
+</html>
